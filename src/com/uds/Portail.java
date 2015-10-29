c4a9043aaeff218fe23b050c2960fc267d6aff50
François Caillet <Francois.caillet@calopea.com>
2015-10-29 16:35:37 +0100

    portail

--- src/com/uds/Portail.java
+++ src/com/uds/Portail.java
@@ -3,6 +3,7 @@ package com.uds;
 import java.lang.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Portail { //PORTAIL_UDS
 
@@ -11,49 +12,128 @@ public class Portail { //PORTAIL_UDS
     public List<Filiere> listGroupFiliere = new ArrayList<Filiere>();
 
     public static void main(String[] args) {
-        //EXEMPLE FACTORY
-        //src: http://www.jmdoudoux.fr/java/dej/chap-design-patterns.htm
-        ////////////////////////////
-        GroupeFactory groupInstitutionnel = new GroupeInstitutionnelFactory();
-        GroupeFactory groupTypique = new GroupeTypiqueFactory();
 
-        Groupe groupe = null;
+        //varible de l'environement
+        Scanner in = new Scanner(System.in);
+        String nom;
+        String prenom;
+        boolean actif = true;
+        String selectedItem;
 
-        System.out.println("Utilisation de la premiere fabrique à groupe institutionnel");
-//        groupe = groupInstitutionnel.getGroupe();
-//        groupe.method();
+        //- Authentification
+        System.out.println("Bienvenue sur le portail UDS.\n=============================");
+        System.out.println("Entrez votre nom:");
+        nom = in.nextLine();
+        System.out.println("Entrez votre prenom:");
+        prenom = in.nextLine();
 
-        System.out.println("Utilisation de la seconde fabrique à groupe typique");
-//        groupe = groupTypique.getGroupe();
-//        groupe.method();
+        //création du membre
+        Membre newUser = new Membre(nom, prenom);
 
-        //EXEMPLE Composite
-        //src: https://en.wikipedia.org/wiki/Composite_pattern
-        ////////////////////////////
-        //Initialize 4 feuilles (doc,service)
-        Document doc1 = new Document();
-        Service service1 = new Service();
-        Document doc2 = new Document();
-        Document doc3 = new Document();
+        //- Système
+        while (actif) {
+            selectedItem = displayMenu(); //affiche le menu
 
-        //Initialize 3 répertoire
-        Repertoire repertoire = new Repertoire();
-        Repertoire sousRepertoire1 = new Repertoire();
-        Repertoire sousRepertoire2 = new Repertoire();
+            switch (selectedItem) {
+                case "1":
+                    String selectedGroup;
+                    List<Groupe> listeGroupe = newUser.getListGroups();
+                    displayGroups(listeGroupe);
+                    System.out.println("Selectionner un groupe:");
+                    selectedGroup = in.nextLine();
 
-        //Composes les répertoires
-        sousRepertoire1.add(doc1);
-        sousRepertoire1.add(service1);
-        sousRepertoire1.add(doc2);
+                    if (Integer.parseInt(selectedGroup) <= listeGroupe.size()) {
+                        Groupe selectGroup = listeGroupe.get(Integer.parseInt(selectedGroup) - 1);
+                        System.out.println("=============================\n" +
+                                "Configuration du groupe \"" + selectGroup.titre  + "\"" +
+                                "\n=============================\n" +
+                                "1- Ajouter un objet au groupe (document, répertoire, service)\n");
+                        String actionSelected = in.nextLine();
+                        switch (actionSelected){
+                            case "1":
+                                System.out.println("Selectionner le type de l'objet:\n" +
+                                        "1-Document\n" +
+                                        "2-Répertoire\n" +
+                                        "3-Service\n");
+                                String typeObject = in.nextLine();
+                                System.out.println("Titre:\n");
+                                String titre = in.nextLine();
+                                System.out.println("Description:\n");
+                                String description = in.nextLine();
+                                createObjet(typeObject);
+                            default:
+                                System.err.println("error");
+                        }
+                    }
+                    break;
+                case "2":
+                    String desc;
+                    String title;
+                    System.out.println("Entrez un titre:");
+                    title = in.nextLine();
+                    System.out.println("Entrez une description:");
+                    desc = in.nextLine();
+                    //Groupe newGroup = newUser.createGroup(title, desc);
+                    //displayGroups(new ArrayList<Groupe>() {{ add(newGroup);}});
+                    break;
+                default:
+                    System.err.println("Saisie incorrecte");
+                    break;
+            }
 
-        sousRepertoire2.add(doc3);
+            System.err.println("Pressez entrer pour revenir au menu, exit pour la quitter");
+            String tmp = in.nextLine();
+            switch (tmp){
+                case "exit":
+                    actif = false;
+                    break;
+                default:
+                    actif = true;
 
-        repertoire.add(sousRepertoire1);
-        repertoire.add(sousRepertoire2);
+            }
 
-        //affiche le répertoire !
-        repertoire.print();
+        }
+        System.err.println("À bientôt sur le portail UDS");
+
+    }
+
+    public static void createObjet(String type){
+        switch (type){
+            case "1":
+
+            case "2":
+
+            case "3":
+
+            default:
+
+        }
+    }
+
+    public static String displayMenu(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Choisissez une action:" +
+                "\n1- Afficher tous les groupes" +
+                "\n2- Créer un groupe");
+        return in.nextLine();
+    }
+
+    public static void displayGroups(List<Groupe> groupes) {
+        int i = 1;
+        if(groupes.size() == 0){
+            System.out.println("Liste Vide");
+            //displayMenu();
+        }else {
+            for (Groupe g : groupes) {
+                System.out.println(i + "-" + g.titre);
+                System.out.println(g.description + "\n---------------------------------");
+
+                i++;
+            }
+        }
 
     }
 
 }
+
+
