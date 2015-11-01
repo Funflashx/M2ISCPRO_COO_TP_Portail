09f7fd218a0e08b537b1013677fd1fc23a4f2a6b
François Caillet <Francois.caillet@calopea.com>
2015-11-01 20:10:29 +0100

    DSI

---
+++ src/com/uds/DSI.java
@@ -0,0 +1,10 @@
+package com.uds;
+
+/**
+ * Created by francoiscaillet on 01/11/2015.
+ */
+public class DSI extends Membre {
+    public DSI(String firstname, String lastname) {
+        super(firstname, lastname);
+    }
+}
